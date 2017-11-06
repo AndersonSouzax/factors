7bf1589733c685c290c6bd47781a04da1cdfe01c
Anderson <anderson.souzax@hotmail.com>
2017-11-06 11:09:36 -0300

    setting up project

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//     entry: "./factors/src/App.js",
+//     output:{
+//         filename: "public/bundle.js"
+//     },
+//     module:{
+//         loaders: [
+//             {
+//                 test: /\.js$/,
+//                 exclude:[/node_modules/,'/spec/'],
+//                 loader: 'babel',
+//                 query: {
+//                     presets: ['react','es2015']
+//                 }
+//             }
+//         ]
+//     }
+// }
